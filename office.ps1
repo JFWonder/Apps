cfbf412520341a4b5deed0db9f155d3e4fa24c00
JFWonder <joao.ferreira@wondercom.pt>
2025-08-22 16:05:40 +0100

    Update office.ps1

--- office.ps1
+++ office.ps1
@@ -1,19 +1,18 @@
 # ===== Instalação de setups .exe =====
 
-# Caminhos ou URLs dos setups
 $office1 = "https://jfwonder.github.io/Apps/officesetup.exe"
 $office2 = "https://jfwonder.github.io/Apps/officetools.exe"
 
-# Caminho temporário para guardar os setups
-$tempPath = "$env:TEMP"
+$tempPath = [System.IO.Path]::GetTempPath()
 
-# Função para baixar e executar setup silenciosamente
 function Install-Exe($url, $filename, $args="/quiet /norestart") {
     $destFile = Join-Path $tempPath $filename
-    Invoke-WebRequest -Uri $url -OutFile $destFile
-    Start-Process -FilePath $destFile -ArgumentList $args -Wait
+    Write-Output "A baixar $filename..."
+    Invoke-WebRequest -Uri $url -OutFile $destFile -UseBasicParsing
+    Write-Output "A instalar $filename..."
+    Start-Process -FilePath $destFile -ArgumentList $args -Wait -PassThru
+    Write-Output "$filename instalado."
 }
 
-# Executar os setups
 Install-Exe $office1 "officesetup.exe"
 Install-Exe $office2 "officetools.exe"
